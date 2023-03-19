aa93b794c9427988fb7255814f14e6f1d7285122
Rafael  Favaro <favarote01@hotmail.com>
2023-03-19 17:55:34 -0300

    atualização com novas funcionalidades

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,22 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+
+interface ButtonProps extends RectButtonProps{
+  color: string;
+}
+
+export const Container = styled(RectButton)<ButtonProps>`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 19px;
+
+  background-color: ${({ color, theme }) => color ? color : theme.colors.main};
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(15)}px;
+`;
---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,75 @@
+import { Accessory } from '../../components/Accessory';
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+
+import { 
+  Container, 
+  Header, 
+  CarImages, 
+  Content, 
+  Details, 
+  Description, 
+  Brand, 
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Acessories,
+  Footer
+} from './styles';
+import { Button } from '../../components/Button';
+
+export function CarDetails() {
+  return (
+    <Container>
+        <Header>
+          <BackButton onPress={()=>{}} />
+            
+        </Header>
+
+      <CarImages>
+        <ImageSlider 
+          imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']} 
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>AUDI</Brand>
+            <Name>RS 5 Coupé</Name>
+          </Description>
+          <Rent>
+            <Period>Ao Dia</Period>
+            <Price>R$ 120</Price>
+          </Rent>
+        </Details>
+        
+        <Acessories>
+          <Accessory name="380Km/h" icon={SpeedSvg}/>
+          <Accessory name="3.2s" icon={AccelerationSvg}/>
+          <Accessory name="800 HP" icon={ForceSvg}/>
+          <Accessory name="Gasolina" icon={GasolineSvg}/>
+          <Accessory name="Auto" icon={ExchangeSvg}/>
+          <Accessory name="2 pessoas" icon={PeopleSvg}/>
+        </Acessories>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+
+      </Content>
+      <Footer>
+        <Button title='Confirmar'/>
+      </Footer>
+    </Container>
+  );
+}
